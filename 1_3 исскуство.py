fe2d485cc23718cc3fd9402e8a814db4f6fbfef0
sodiki366 <mark1704kos@gmail.com>
2025-02-15 08:44:04 +0300

    ddd

--- 1_3 исскуство.py
+++ 1_3 исскуство.py
@@ -2,13 +2,21 @@
 from all_colors import *
 import random
 import pygame
-
 pygame.init()
+# pygame.mixer.init()
+# pygame.mixer.music.load('resurs/La La Land.mp3')
+# pygame.mixer.music.play(-1)
+
 size = (1280, 720)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("My Game")
+COLORS = [RED,GREEN, BLUE, YELLOW, CYAN, MAGENTA, GRAY, ORANGE, PINK, BROWN , PURPLE, LIME, NAVY, OLIVE, MAROON, TEAL, SILVER, GOLD]
 BACKGROUND = (255, 255, 255)
 screen.fill(BACKGROUND)
+line_color = RED
+line_width = 5
+start_pos = (0, size[1]//2)
+end_pos = (size[0], size[1]//2)
 
 FPS = 60
 clock = pygame.time.Clock()
@@ -19,10 +27,16 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     # Основная логика игры
+    BACKGROUND = random.choice(COLORS)
+
     # Отрисовка объектов
-
-
     screen.fill(BACKGROUND)#очистка экрана
+
+
+
+
+
+
     pygame.display.flip()
     clock.tick(FPS)
 
